c0dfc99330ad890c7eaa09e82ab0bf8b9ae85fbd
조경진 <cho881020@gmail.com>
2021-07-10 10:27:46 +0900

    로그 찍어보기

--- app/src/main/java/com/neppplus/kotlinbasic_20210710/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20210710/MainActivity.kt
@@ -2,10 +2,21 @@ package com.neppplus.kotlinbasic_20210710
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        테스트 버튼이 눌리면 할 일을 지정해주자.
+        testBtn.setOnClickListener {
+
+//            버튼이 눌리는지 로그를 찍어보자.
+            Log.d("메인화면", "테스트 버튼 클릭됨")
+
+        }
+
     }
 }
